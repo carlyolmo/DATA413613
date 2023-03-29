48c5fbf2ff40f054c96398789db504536646ae20
carlyolmo <112495148+carlyolmo@users.noreply.github.com>
2023-03-29 17:44:59 -0400

    Add files via upload

---
+++ DS413613ClassworkLabForkingRfile.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+mpg
+# My name is Carly Olmo
+# The data source is the mpg data table
+
+ggplot(data = mpg) +
+  geom_boxplot(mapping = aes(x = displ, y = drv, fill = drv)) +
+  ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
+  xlab("displacement") +
+  ylab("transmission types") 
+
+
+
+
